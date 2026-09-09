35d6071b917321ad91ed1697351d8a56f1b6a792
Mirko Onorato <85618159+mirkoSkF@users.noreply.github.com>
2026-09-09 14:59:00 +0200

    Add files via upload

--- src/main/java/it/skillfactory/eco/config/SecurityConfig.java
+++ src/main/java/it/skillfactory/eco/config/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
                     "/images/**",
                     "/tinymce/**",
                     "/uploads/**",
+                    "/fonts/**",
                     "/api/uploads/**",
                     "/favicon.ico",
                     "/p/**"
